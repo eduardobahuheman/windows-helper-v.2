d8b182b0920346735f8dc07ec0e6daec26232fbf
eduardobahuheman <edu03.bahuheman@gmail.com>
2024-10-23 21:36:14 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///questions.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Carregar arquivo JSON
+with open('questions.json', 'r') as f:
+    questions_data = json.load(f)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/question/<int:id>')
+def question(id):
+    question = next((q for q in questions_data['questions'] if q['id'] == id), None)
+    if 'result' in question:
+        return render_template('result.html', result=question['result'])
+    return render_template('question.html', question=question)
+
+if __name__ == '__main__':
+    app.run(debug=True)
